ee78828ce756705260516975cf48a0f7069a2500
JustinJefferson <justleland@gmail.com>
2019-10-24 16:12:49 -0400

    Created a Tile Model

--- TacticalRPG/Assets/Models/Tile.cs
+++ TacticalRPG/Assets/Models/Tile.cs
@@ -2,17 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tile : MonoBehaviour
+public class Tile
 {
-    // Start is called before the first frame update
-    void Start()
+    private int _x;
+    private int _y;
+    bool _passable;
+    public GameObject Resident { get; set; }
+    
+
+    public Tile(int x, int y, bool passable, GameObject resident) 
     {
-        
+        this._x = x;
+        this._y = y;
+        this._passable = passable;
+        this.Resident = resident;
     }
 
-    // Update is called once per frame
-    void Update()
+    public int X { get { return _x; } }
+    public int Y { get { return _y; } }
+
+    public bool IsPassable { get { return (_passable && Resident != null); } }
+
+    public Unit ResidentUnit
     {
-        
+        get 
+        {
+            if(Resident != null) 
+            {
+                Unit unit = Resident.GetComponent<Unit>();
+                if(unit != null) return unit;
+            }
+            return null;
+        }
     }
+
+    
 }
